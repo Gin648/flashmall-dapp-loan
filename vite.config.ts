fe9862bbd0c40946ed616440a1338cc9207ac561
Gin648 <89142935+lin1126@users.noreply.github.com>
2024-10-15 19:03:16 +0800

    feat: 线上打包

--- vite.config.ts
+++ vite.config.ts
@@ -20,7 +20,7 @@ export default defineConfig(({ mode }) => {
   // 环境变量
   const env = loadEnv(mode, root, "");
   return {
-    base: env.VITE_PUBLIC_PATH || "/",
+    base: "/tttt/",
     plugins: [
       vue(),
       vueJsx(),
