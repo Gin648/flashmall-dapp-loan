b7bfe41a7e58c3ce4c46c4c6b9f8793ccc46902d
Gin648 <89142935+lin1126@users.noreply.github.com>
2024-10-15 15:34:40 +0800

    feat: router

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,30 @@
 <template>
-  <router-view />
+  <div class="app-wrapper text-base">
+    <router-view v-slot="{ Component }">
+      <keep-alive :include="cachedViews">
+        <component :is="Component" />
+      </keep-alive>
+    </router-view>
+  </div>
 </template>
 
-<style></style>
+<script setup lang="ts">
+import { useCachedViewStoreHook } from "@/store/modules/cachedView";
+import { useDarkMode } from "@/hooks/useToggleDarkMode";
+import { computed } from "vue";
+const cachedViews = computed(() => {
+  return useCachedViewStoreHook().cachedViewList;
+});
+</script>
+
+<style lang="less" scoped>
+@import "@/styles/mixin.less";
+
+.app-wrapper {
+  .clearfix();
+  position: relative;
+  height: 100%;
+  width: 100%;
+  overflow-y: auto;
+}
+</style>
--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,37 +1,17 @@
-import Layout from "@/layout/index.vue";
 import type { RouteRecordRaw } from "vue-router";
-import Demo from "@/views/demo/index.vue";
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: "/",
     name: "root",
-    component: Layout,
-    redirect: { name: "Demo" },
+    redirect: { name: "LpPledge" },
     children: [
       {
-        path: "demo",
-        name: "Demo",
-        component: Demo,
+        path: "lp-pledge",
+        name: "LpPledge",
+        component: () => import("@/views/LpPledge/index.vue"),
         meta: {
-          title: "主页"
-        }
-      },
-      {
-        path: "tools",
-        name: "Tools",
-        component: () => import("@/views/tools/index.vue"),
-        meta: {
-          title: "工具"
-        }
-      },
-      {
-        path: "about",
-        name: "About",
-        component: () => import("@/views/about/index.vue"),
-        meta: {
-          title: "关于",
-          noCache: true
+          title: "lp质押"
         }
       }
     ]
