2a3e9fadd982111e4a9f6e3ea5cf0f4d3498c6b5
Gin648 <89142935+lin1126@users.noreply.github.com>
2024-10-31 17:30:12 +0800

    perf: 环境配置

--- vite.config.ts
+++ vite.config.ts
@@ -20,7 +20,7 @@ export default defineConfig(({ mode }) => {
   // 环境变量
   const env = loadEnv(mode, root, "");
   return {
-    base: "/tttt/",
+    base: env.VITE_PUBLIC_PATH || "/",
     plugins: [
       vue(),
       vueJsx(),
