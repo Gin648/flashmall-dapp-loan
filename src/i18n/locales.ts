7d47ab784ce71277b44d218b5fed03122120a6a6
wutingtingheimei <74999356+wutingtingheimei@users.noreply.github.com>
2024-11-25 21:26:42 +0800

    fix:样式

--- src/i18n/locales.ts
+++ src/i18n/locales.ts
@@ -1,6 +1,6 @@
 import zhCNLocale from "./lang/zh-HK.json";
 import enLocale from "./lang/en-US.json";
-import jaLocale from "vant/es/locale/lang/ja-JP";
+import jaLocale from "./lang/ja-JP.json";
 
 const locales = {
   "zh-HK": zhCNLocale,
