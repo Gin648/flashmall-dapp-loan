4dc394838813f39828332e867b83de1ee2ab47b7
wutingtingheimei <74999356+wutingtingheimei@users.noreply.github.com>
2024-12-02 16:07:34 +0800

    fix:commonpop

---
+++ src/components/CommonPop.vue
@@ -0,0 +1,41 @@
+<template>
+  <van-popup
+    :show="show"
+    teleport="body"
+    :position="position"
+    :styles="styles"
+    :close-on-click-overlay="false"
+    @open="() => emit('open')"
+  >
+    <div class="w-full h-[100%] p-5 bg-[#FEFFF9] overflow-hidden">
+      <slot name="header" />
+      <slot name="content" />
+    </div>
+  </van-popup>
+</template>
+
+<script setup lang="ts">
+import { PopupPosition } from "vant";
+
+withDefaults(
+  defineProps<{
+    show: boolean;
+    name?: any;
+    width?: string;
+    noClose?: boolean;
+    noHeader?: boolean;
+    noBorder?: boolean;
+    position?: PopupPosition;
+    styles?: any;
+  }>(),
+  {
+    position: "center",
+    styles: {
+      width: "80%"
+    }
+  }
+);
+const emit = defineEmits(["close", "open"]);
+</script>
+
+<style scoped lang="scss"></style>
--- src/views/LpPledge/index.vue
+++ src/views/LpPledge/index.vue
@@ -4,6 +4,9 @@
   <BackIcon />
   {{ $t("zi-chan") }}
   MC:{{ amount }}
+  <VanButton @click="setPopShow(true)">
+    <span class="text-[#2E2008] text-base"> 侧边栏 </span>
+  </VanButton>
   <ApproveButton
     :loading="loading"
     :approveAmount="10"
@@ -14,6 +17,15 @@
   >
     <span class="text-[#2E2008] text-base"> 转帐 </span>
   </ApproveButton>
+  <CommonPop
+    :show="true"
+    position="right"
+    :styles="{ width: '50%', height: '100%' }"
+  >
+    <template v-slot:content>
+      <div>侧边栏内容</div>
+    </template>
+  </CommonPop>
 </template>
 
 <script setup lang="ts">
@@ -21,9 +33,11 @@ import { ref, onMounted } from "vue";
 import { erc20Contract } from "@/contract/erc20Contract";
 import { config } from "@/config";
 import useStore from "@/store";
+import CommonPop from "@/components/CommonPop.vue";
 import ApproveButton from "@/components/ApproveButton.vue";
 import flashMallContract from "@/contract/flashMallContract";
 import BackIcon from "@/components/BackIcon.vue";
+import { useToggle } from "@vueuse/core";
 const { accountStore } = useStore();
 const amount = ref(0);
 const getBalance = async () => {
@@ -31,12 +45,12 @@ const getBalance = async () => {
   amount.value = +(await balanceOf());
 };
 
-const { trade } = flashMallContract();
+const [popShow, setPopShow] = useToggle(false);
 
 const loading = ref(false);
 const submitFrom = async () => {
   loading.value = true;
-  await trade("0x4E4B027778C48EA8A43e4e6473D8C630158B51e2", 10);
+  // await trade("0x4E4B027778C48EA8A43e4e6473D8C630158B51e2", 10);
   loading.value = false;
 };
 onMounted(() => {
